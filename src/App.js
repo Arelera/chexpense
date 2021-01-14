190bda55d4d1e1d681de635729769d0151ee4c04
Arel <arelcode@gmail.com>
2021-01-14 23:59:45 +0300

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Yo</div>;
+}
+
+export default App;
---
+++ src/theme/globalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 62.5%;
+  }
+`;
+export default GlobalStyles;
